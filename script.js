643ba91897050ac44540300c5d6b69e0f1d01812
Aloha <utah.clayton@gmail.com>
2021-06-15 23:43:31 -0700

    issue generating search history array, revisit

--- script.js
+++ script.js
@@ -5,16 +5,17 @@
 
 $(document).ready(function() {
     $("#search-btn").on("click", function () {
-        var city = $("#city-input").val();
+        var city = $("#city-input").val().trim();
+        $("#city-input").val("");
         var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q="+ city +"&units=imperial&appid=d81f33c887aad9cbe02d418796972cb8";
-        fetch(apiUrl).then(function(res){
-            return res.json()
-        }).then(function(res){
-            var lon = res.coord.lon;
-            var lat = res.coord.lat;
-            var url = "https://api.openweathermap.org/data/2.5/onecall?lat="+ lat +"&lon="+ lon +"&units=imperial&appid=d81f33c887aad9cbe02d418796972cb8"
-            fetch(url).then(function(res){
-                return res.json()
+        fetch(apiUrl).then(function(response){
+            return response.json()
+        }).then(function(response ){
+            var lon = response.coord.lon;
+            var lat = response.coord.lat;
+            var url = "https://api.openweathermap.org/data/2.5/onecall?lat="+ lat +"&lon="+ lon +"&units=imperial&appid=d81f33c887aad9cbe02d418796972cb8";
+            fetch(url).then(function(response){
+                return response.json()
             }).then(function(data){
                 console.log(data)
                 //render to html
@@ -24,17 +25,31 @@ $(document).ready(function() {
     
 });
 
-// attempt to render to html
-// var renderCity = function() {
-//     var searchedCity = $("#searched-city");
-//     searchedCityt.html("")
-//     for (var i = 0; i < locationArr.length; i++) {
-//         var location = $("<button></button>")
-//             .attr("id", "searched-city-" + i)
-//             .addClass("list-group-item list-group-item-action prev-loc")
-//             .text(locationArr[i]);
-//         searchedCity.append(location);
+//attempt to render to html
+// var cityArr = function(city) {
+//     if (searchHistory.includes(city)) {
+//         addLocation();
+//     } else if (searchHistory.length < 5) {
+//         searchHistory.unshift(city);
+//         addLocation();
+//     } else if (searchHistory.length === 5) {
+//         searchHistory.pop();
+//         searchHistory.unshift(city);
+
 //     }
-// }
+// };
+
+// // var addLocation = function() {
+// //     var searchList = $("#search-history");
+// //     searchList.html("")
+// //     for (var i = 0; i < searchHistory.length; i++) {
+// //         var location = $("<button></button>")
+// //         .attr("id", "searched-", + i)
+// //         .addClass(list-item)
+// //         .text(searchHistory[i]);
+// //         searchList.append(location);
+// //     }
+// // };
+
 
 // searchFormEl.addEventListener("submit",CityInputFormClick);
